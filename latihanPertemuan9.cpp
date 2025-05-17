ee4a6a7aa0252e49c8fcf3973c3596e8e15e916a
MuhammadRioAprilianto <rma994636@gmail.com>
2025-05-17 19:01:51 +0700

    membuat class buku yang berisi atribut judul, penulis dan status buku dipinjam. juga menambahkan friend class petugas

--- latihanPertemuan9.cpp
+++ latihanPertemuan9.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+class petugas;
+class peminjam;
+class admin;
+class buku
+{
+    private:
+    string judul;
+    string penulis;
+    bool dipinjam;
+    public:
+    buku(string j, string p):judul(j), penulis(p), dipinjam(false){}
+    friend class petugas;
+};
 
